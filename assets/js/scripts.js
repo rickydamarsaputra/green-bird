7559d70db8c7a9af1a2cc797c41cf85b0799a586
rickydamarsaputra <retrocode.rc@gmail.com>
2022-11-23 19:19:13 +0700

    fix: mengubah warna

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,10 +27,12 @@ data_pura = {
 };
 var list = document.getElementById('gal');
 list.innerHTML = '';
-for (var i = 1; i <= 23; i++)
-	list.innerHTML += `\n    <div class="col-lg-3 col-sm-6 mb-4">\n        <div class="container-img rounded">\n            <img class="img-fluid image rounded shadow" src="assets/images/gallery/${i}.jpg" alt="${
+for (var i = 1; i <= 23; i++) {
+	list.innerHTML += `\n<div class="col-lg-3 col-sm-6 mb-4">\n<div class="container-img rounded">\n<img class="img-fluid image rounded shadow" src="assets/images/gallery/${i}.jpg" alt="${data_pura.keterangan[i - 1]}" />\n<div class="middle">\n<div class="text">${
 		data_pura.keterangan[i - 1]
-	}" />\n            <div class="middle">\n                <div class="text">${data_pura.keterangan[i - 1]}</div>\n            </div>\n        </div>\n    </div>\n    `;
+	}</div>\n</div>\n</div>\n</div>\n`;
+}
+
 Promise.all(
 	Array.from(document.images)
 		.filter((a) => !a.complete)
